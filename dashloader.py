189b2726c58a456d9dc705e19b97c94f0c0e5530
henley-regatta <42374576+henley-regatta@users.noreply.github.com>
2023-02-13 17:23:03 +0000

    got remote-retrieve-of-raw working, got influx query working

---
+++ dashloader.py
@@ -0,0 +1,194 @@
+# A PiPico uPython script to periodically reload a
+# raw "EPD" format image from a web location
+# which allows "server side rendering" on this 
+# platform with limited memory.
+#
+# To load this at boot, rename to main.py
+#
+# Note that "WaveShareEpaper42.py" and "dashloadercfg.json"
+# must also be in the root of the Pico's filesystem for this to work.
+#
+# an EPD is a custom zlib compressed bytestream
+# of exactly 400x300 pixels, uncompressing to a 
+# bytestream matching the EPD 4Gray (black/darkgrey/greyish/white)
+# *values*. (0x00,0xaa,0x55,00xff yes I know the middle are reversed that's deliberate)
+#
+# Note the config values for network, source URL and refresh rate (in seconds) are 
+# loaded from an external JSON. 
+#
+# There is VERY little error checking done here....
+###########################################################
+import gc
+
+def pMem(why) :
+    print(f"{why}: {gc.mem_alloc()} / {gc.mem_free()}")
+import WaveShareEpaper42
+import utime
+import urequests
+import uio
+import json 
+import zlib
+import machine
+led = machine.Pin("LED", machine.Pin.OUT)
+
+import network
+cfg=json.load(open('dashloadercfg.json','r'))
+
+#Setup globals:
+MAXWIDTH=WaveShareEpaper42.EPD_WIDTH
+MAXHEIGHT=WaveShareEpaper42.EPD_HEIGHT
+CHARWIDTH=8
+CHARHEIGHT=8
+#Bit poor form but treat this as a global:
+epd=WaveShareEpaper42.EPD_4in2()
+
+###########################################################
+def blinkLED(count, onMS) :
+    for x in range(count*2) :
+        led.toggle()
+        utime.sleep_ms(onMS)
+    led.off()
+
+########################################
+def wait_for_wifi():
+    global wlan
+    max_wait = 10
+    while max_wait > 0:
+        blinkLED(1,100)
+        s=wlan.status()
+        if s < 0 or s >= 3:
+            break
+        max_wait -= 1
+        utime.sleep_ms(1000)
+    return s
+
+###########################################################
+def centreText(txt,maxWidth) :
+    tLen = len(txt) * CHARWIDTH
+    border = maxWidth - tLen
+    if border <= 0 :
+        return 0
+    else :
+        return int(border / 2)
+
+###########################################################
+def errDumpText(texttodump) :
+    global epd
+    gc.collect() # voodoo
+    epd.EPD_4IN2_Init()
+    #Initialise the framebuffer to white:
+    epd.image4Gray.fill(0xff)
+
+    maxCharsPerLine=int(MAXWIDTH/CHARWIDTH)-2*CHARWIDTH
+    errLines=list(texttodump[0+i:maxCharsPerLine+i] for i in range(0,len(texttodump),maxCharsPerLine))
+    tHeight=int(MAXHEIGHT/2)-len(errLines)*CHARHEIGHT+2
+    
+    for errLine in errLines :
+        epd.image4Gray.text(errLine,centreText(errLine,MAXWIDTH),tHeight,epd.black)
+        tHeight += CHARHEIGHT+2
+    epd.EPD_4IN2_4GrayDisplay(epd.buffer_4Gray)
+    epd.Sleep()
+    epd.reset()
+    epd.module_exit()
+    gc.collect() # voodoo
+
+########################################
+def get_gscale(gscale_url): 
+    try:
+        print(f"Getting Image Data from {gscale_url}")
+        hdrs={"Accept" : "application/octet-stream",
+              "User-Agent" : "dashloader/0.1"}
+        r = urequests.get(gscale_url,headers=hdrs)
+        if r.status_code >= 200 and r.status_code < 300 :
+            print(f"Request OK with rc={r.status_code}, content-size: {len(r.content)}")
+            buff=r.content
+            r.close() #bad things happen if you don't close the request
+            return(buff)
+        else :
+            rc=r.status_code
+            r.close()
+            raise IOError(f'GET of {gscale_url} failed with response code = {rc}')
+        #If we got here, we failed. Own it.
+        raise IOError(f'Unknown error retrieving {gscale_url}')
+    except Exception as err:
+        errType = type(err).__name__
+        errString=(f"Exception retrieving {gscale_url} : {errType}\n{err}")
+        print(errString)
+        for x in range(5) :
+            blinkLED(3,250)
+            utime.sleep_ms(500)
+            blinkLED(3,666)
+            utime.sleep_ms(500)
+            blinkLED(3,250)
+            utime.sleep_ms(1000)
+
+########################################
+def display_img(imgdata) :
+   #Blank the ePaper
+    epd.EPD_4IN2_Init()
+    #Initialise the framebuffer to white:
+    epd.image4Gray.fill(0xff)
+
+    psuid = uio.BytesIO(imgdata)
+    decomp = zlib.DecompIO(psuid)
+    for y in range(MAXHEIGHT) :
+        lData = decomp.read(MAXWIDTH)
+        if lData :
+            for x in range(MAXWIDTH) :
+                epd.image4Gray.pixel(x,y,lData[x])
+            
+    epd.EPD_4IN2_4GrayDisplay(epd.buffer_4Gray)
+    epd.Sleep()
+        
+########################################
+# This is important in giving a
+# window of control - once we get to
+# system.lightsleep() we lose control of
+# REPL - this isn't just diagnostics it's
+# an opportunity to break out
+blinkLED(10,250)
+initTxt = f"Refresh every {cfg['SLEEPINTERVAL']/60} minutes for {cfg['SERVERURL']} connecting to {cfg['SSID']}"
+print(initTxt)
+errDumpText(initTxt)        
+########################################
+# Main program loop:
+while True:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(cfg['SSID'],cfg['SSPASS'])
+    while (wait_for_wifi() != 3) :
+        utime.sleep_ms(1000)
+        wlan.connect(cfg['SSID'],cfg['SSPASS'])
+
+    #Indicator of connection established:
+    blinkLED(2,1000)
+
+    # PAYLOAD GOES HERE
+    try:
+        display_img(get_gscale(cfg['SERVERURL']))
+    except TypeError :
+        print(f"Failed to get/display image. I Sleep.")
+  
+
+    #If proposing to system.lightsleep(), epd.Sleep() isn't enough
+    #one needs some more drastic disconnect:
+    epd.reset()
+    epd.module_exit()
+
+    #Indicator of display complete:
+    blinkLED(10,100)
+    
+    wlan.disconnect()
+    wlan.active(False)
+    #https://github.com/orgs/micropython/discussions/9135
+    #vital for actually turning wifi off:
+    wlan.deinit()
+    wlan = None
+    blinkLED(1,10) ##effectively an "off"
+    #note that the ePaper needs to be "disconnected" before
+    #sleep to prevent a dim screen (pin going high?).
+    #check ePaper code not only does epd.Sleep() but also
+    #epd.reset()/epd.module_exit() before we get to this
+    #point:
+    machine.idle() 
+    machine.lightsleep(cfg['SLEEPINTERVAL'] * 1000)
--- demosvr.py
+++ demosvr.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python3
-#
-# dashsvr.py
-# ----------
-# A simple web server that only serves up images constructed on-the-fly
-# in low-res low-colour of a power dashboard.
-#
-# Intended as a server for a PiPico ePaper display that doesn't have the 
-# memory to build it's own image so needs a server-supplied one.
-#
-# NON-STANDARD MODULES REQUIRED:
-#   pip install influxdb-client
-#########################################################################
 import socket,threading,socketserver,sys,time,zlib
 from datetime import datetime
 import pytz
-import json
-from PIL import Image, ImageFilter, ImageDraw, ImageOps, ImageFont
+from PIL import Image, ImageFilter, ImageShow, ImageOps, ImageDraw, ImageFont
 
-cfg=json.load(open('dashsvrcfg.json','r'))
-
-#Load our values from the cfg
 # SETTINGS FOR THE GENERATED IMAGE:
-IMGWIDTH  = cfg['IMGWIDTH']
-IMGHEIGHT = cfg['IMGHEIGHT']
+IMGWIDTH  = 400
+IMGHEIGHT = 300
 #AVAILABLE "COLOURS" IN THE IMAGE LISTED FROM DARK->LIGHT
-IMGCOLS = cfg['IMGCOLS']
+IMGCOLS = [0x00,0xaa,0x55,0xff]
 
 #PORT TO LISTEN ON
-LISTENPORT = cfg['SVRPORT']
-#TIMEZONE for Queries and stuff:
-TZ=pytz.timezone(cfg['TIMEZONE'])
-#BASE URL for Influx queries:
-querybase = f"http://{cfg['INFLUXSVR']}:{cfg['INFLUXPORT']}/query?db={cfg['INFLUXDB']}&u={cfg['INFLUXUSER']}&p={cfg['INFLUXPASS']}&q="
+LISTENPORT = 7478
+TZ = pytz.timezone('Europe/London')
 
-
+demoimg="beach.jpg"
+svrdata=bytearray()
 
 #########################################################################
 def gen_image(width,height) :
@@ -47,23 +29,28 @@ def gen_image(width,height) :
     draw.multiline_text((width/2,height/2), "Hello\nWorld!",font=myFont, anchor="ms",fill=0x00)
     
     return img
-   
+
 #########################################################################
 # Here's our "web server" handler
 class ThreadedTCPRequestHandler(socketserver.StreamRequestHandler) :
     def handle(self):
+        
         self.data = self.rfile.readline().strip()
         p = str(self.data).split()
-        print(f"{self.client_address[0]} requested: {p[1]}")
-        stdhdrs=f"HTTP/1.1 200 OK\r\nServer: dashsvr/0.1\r\nAccept-Ranges: bytes\r\nDate: {datetime.now(TZ)}"
+        print(f"{self.client_address[0]} requested: {p[1]} ({p})")
+        stdhdrs=f"HTTP/1.1 200 OK\r\nServer: demosvr/0.1\r\nAccept-Ranges: bytes\r\nDate: {datetime.now(TZ)}"
+        
         if len(p)>0 and ("png" in p[1].lower()) :
             print("Writing PNG")
-            self.wfile.write(b'HTTP/1.0 200 OK\nContent-Type: image/png\n\n')
+            hdr = f"{stdhdrs}\r\nContent-Type: image/png\r\n\r\n"
+            self.wfile.write(bytes(hdr,encoding="utf-8"))
             im = gen_image(IMGWIDTH,IMGHEIGHT)
             im.save(self.wfile,format='PNG')
         else :
             print("Writing RAW")
-        
+            hdr = f"{stdhdrs}\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(svrdata)}\r\n\r\n"
+            self.wfile.write(bytes(hdr,encoding="utf-8"))
+            self.wfile.write(svrdata)
 #########################################################################
 # Here's our "Web Server":
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -73,12 +60,32 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 #########################################################################
 #########################################################################
 if __name__ == "__main__" :
+    
+    with Image.open(demoimg) as pic:
+        pic.thumbnail((IMGWIDTH,IMGHEIGHT))
+        greypic = ImageOps.grayscale(pic)
+        twobitpic = ImageOps.posterize(greypic,2).tobytes(encoder_name='raw')
+        epd4gray = bytearray()
+        for b in range(len(twobitpic)) :
+            v = twobitpic[b]
+            if v == 0 :
+                epd4gray.append(0x00)
+            elif v == 64 :
+                epd4gray.append(0xaa)   # NOTE: this is deliberately in the wrong order;
+            elif v == 128 :
+                epd4gray.append(0x55)   # for reasons unknown, "light grey" has a lower byte index than "dark gray"
+            else :
+                epd4gray.append(0xff)
+        print(f"Posterised raw image length = {len(epd4gray)}")
+        svrdata=zlib.compress(epd4gray)
+    print(f"zLib compressed data size = {len(svrdata)} bytes")
+    
     socketserver.TCPServer.allow_reuse_address = True #needed to allow restart after Ctrl-C
     server = ThreadedTCPServer(("",LISTENPORT), ThreadedTCPRequestHandler)
     with server :
         ip, port = server.server_address
         server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True 
+        server_thread.daemon = True
         server_thread.start()
         print(f"Listening at {ip} on port {port} via {server_thread.name}")
         try:
@@ -88,7 +95,3 @@ if __name__ == "__main__" :
             print("Intercepted Ctrl-C")
             server.shutdown()
             sys.exit()
-
-
-        
-
